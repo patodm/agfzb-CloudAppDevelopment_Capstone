f5f256dd667b80e8eb92e47213faa833a8f8f98d
Patricio Díaz <diaz.m.patricio@gmail.com>
2022-09-28 20:49:11 -0400

    Update Lab User Management

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,11 +16,11 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
 
     # path for registration
-
+    path('registration/', views.registration_request, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
-
+    path('logout/', views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
